b017abc0830086de5779f2e743feca86151ab006
Joakim Unge <ashba@unge.se>
2020-05-16 16:57:12 +0200

    refactor: update demo screen

--- 404.ts
+++ 404.ts
@@ -8,58 +8,64 @@ export default (resource: string) =>
     <title>404</title>
   </head>
   <style>
-    html,
-    body {
-      height: 100%;
-      width: 100%;
-      font-family: Helvetica, Arial, sans-serif;
-      box-sizing: border-box;
-      background-color: #22212c;
-    }
-
-    #denoliver {
-      width: 100%;
-      height: 100%;
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-    }
-
-    #denoliver > h1 {
-      font-size: 48px;
-      margin-bottom: 0;
-      color: #f6fafe;
-      text-shadow: 0px 3px 15px rgba(0, 0, 0, 0.2);
-    }
-
-    #denoliver > p {
-      color: #89b3dd;
-    }
-
-    #denoliver > div {
-      display: flex;
-    }
-
-    #denoliver span {
-      width: 12px;
-      height: 12px;
-      border-radius: 50%;
-      background-color: #89b3dd;
-      margin-right: 16px;
-    }
-
-    #denoliver span:nth-child(1) {
-      background-color: #d4a7f1;
-    }
-
-    #denoliver span:nth-child(3) {
-      background-color: #a2eee9;
-    }
-
-    #denoliver span:last-child {
-      margin-right: 0;
-    }
+  html,
+  body {
+    height: 100%;
+    width: 100%;
+    font-family: Helvetica, Arial, sans-serif;
+    box-sizing: border-box;
+    background-color: #fff;
+  }
+  
+  #denoliver {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+  
+  #denoliver > h1 {
+    font-size: 48px;
+    margin-bottom: 0;
+    color: #726f7b;
+  }
+  
+  #denoliver > p {
+    color: #c4c4c4;
+    text-align: center;
+  }
+  
+  #denoliver > div {
+    display: flex;
+  }
+  
+  #denoliver span {
+    width: 12px;
+    height: 12px;
+    border-radius: 50%;
+    background-color: #89b3dd;
+    margin-right: 16px;
+  }
+  
+  #denoliver span:nth-child(1) {
+    background-color: #d4a7f1;
+  }
+  
+  #denoliver span:nth-child(3) {
+    background-color: #a2eee9;
+  }
+  
+  #denoliver span:last-child {
+    margin-right: 0;
+  }
+  
+  strong {
+    opacity: 0.2;
+    font-weight: 200;
+  }
+  
   </style>
   <body>
     <div id="denoliver">
