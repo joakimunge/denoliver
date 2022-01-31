55802bff394c04d1870a8e38ad3aa6828d1e9e3a
Joakim Unge <ashba@unge.se>
2022-01-31 11:53:55 +0100

    chore: update standard lib to 0.97

--- deps.ts
+++ deps.ts
@@ -1,23 +1,23 @@
-export { parse } from 'https://deno.land/std@0.81.0/flags/mod.ts'
-export { acceptWebSocket } from 'https://deno.land/std@0.81.0/ws/mod.ts'
+export { parse } from 'https://deno.land/std@0.97.0/flags/mod.ts'
+export { acceptWebSocket } from 'https://deno.land/std@0.97.0/ws/mod.ts'
 export {
   serve,
   Server,
   serveTLS,
   ServerRequest,
-} from 'https://deno.land/std@0.81.0/http/server.ts'
+} from 'https://deno.land/std@0.97.0/http/server.ts'
 export {
   blue,
   bold,
   green,
   red,
-} from 'https://deno.land/std@0.81.0/fmt/colors.ts'
-export { posix } from 'https://deno.land/std@0.81.0/path/mod.ts'
-export { extname } from 'https://deno.land/std@0.81.0/path/mod.ts'
+} from 'https://deno.land/std@0.97.0/fmt/colors.ts'
+export { posix } from 'https://deno.land/std@0.97.0/path/mod.ts'
+export { extname } from 'https://deno.land/std@0.97.0/path/mod.ts'
 export {
   assert,
   assertEquals,
-} from 'https://deno.land/std@0.81.0/testing/asserts.ts'
-export { TextProtoReader } from 'https://deno.land/std@0.81.0/textproto/mod.ts'
-export { BufReader } from 'https://deno.land/std@0.81.0/io/bufio.ts'
-export type { Args } from 'https://deno.land/std@0.81.0/flags/mod.ts'
+} from 'https://deno.land/std@0.97.0/testing/asserts.ts'
+export { TextProtoReader } from 'https://deno.land/std@0.97.0/textproto/mod.ts'
+export { BufReader } from 'https://deno.land/std@0.97.0/io/bufio.ts'
+export type { Args } from 'https://deno.land/std@0.97.0/flags/mod.ts'
