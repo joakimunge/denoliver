05919f2b0d338738103c84b7e2cbd59394dd3278
Joakim Unge <ashba@unge.se>
2025-07-24 12:34:31 +0200

    Format deps.ts imports on single line

--- deps.ts
+++ deps.ts
@@ -1,12 +1,7 @@
 export { assert } from '@std/assert/assert'
 export { assertEquals } from '@std/assert/equals'
 export { parseArgs as parse } from '@std/cli/parse-args'
-export {
-  blue,
-  bold,
-  green,
-  red,
-} from '@std/fmt/colors'
+export { blue, bold, green, red } from '@std/fmt/colors'
 export { extname } from '@std/path/extname'
 export * as posix from '@std/path/posix'
 
